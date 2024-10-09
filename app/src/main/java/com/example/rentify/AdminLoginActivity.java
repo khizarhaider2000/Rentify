9fd920b1cb4b676d1f53b3f580764e33f8ba3a3a
Prithvi <sprithvi2610@gmail.com>
2024-10-08 21:03:49 -0400

    Added some functionality to Admin login

--- app/src/main/java/com/example/rentify/AdminLoginActivity.java
+++ app/src/main/java/com/example/rentify/AdminLoginActivity.java
@@ -1,24 +1,52 @@
 package com.example.rentify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AdminLoginActivity extends AppCompatActivity {
+
+    private static final String ADMIN_USERNAME = "admin";
+    private static final String ADMIN_PWD = "XPI76SZUqyCjVxgnUjm0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        EditText UsernameInput = findViewById(R.id.UsernameInput);
+        EditText PasswordInput = findViewById(R.id.PasswordInput);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enteredUsername = UsernameInput.getText().toString();
+                String enteredPassword = PasswordInput.getText().toString();
+
+                // Check if the entered password matches the admin password
+                if (enteredUsername.equals(ADMIN_USERNAME) && enteredPassword.equals(ADMIN_PWD)) {
+                    Toast.makeText(AdminLoginActivity.this, "Admin logged in!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AdminLoginActivity.this, "Incorrect username/password.", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
